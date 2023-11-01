787b172b2bf2638655f5e6f95b535ae850666b2c
Daniel Souza <workdndsza@gmail.com>
2023-10-31 21:01:57 -0300

    criando o model

---
+++ src/main/java/com/dnsouzadev/apiviacep/service/ViaCepService.java
@@ -0,0 +1,11 @@
+package com.dnsouzadev.apiviacep.service;
+
+import com.dnsouzadev.apiviacep.model.Address;
+
+public class ViaCepService {
+    public Address getAddress(String cep) {
+        Address address = null;
+
+
+    }
+}
